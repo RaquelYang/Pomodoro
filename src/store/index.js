5a6df3b94108316743a5c5c8166f4ef2d34b4d69
aa <lorehmmoreh007@gmail.com>
2022-02-25 11:55:20 +0800

    fix issue

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -70,5 +71,6 @@ export default new Vuex.Store({
     changeitem (state, data) {
       state.current = data
     }
-  }
+  },
+  plugins: [createPersistedState({ key: 'pomodoro' })]
 })
